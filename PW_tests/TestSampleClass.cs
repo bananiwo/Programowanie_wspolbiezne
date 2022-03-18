0e3e734a9da41e18007ca8bcff7aa7a59ade4cde
Magdalena <228932@edu.p.lodz.pl>
2022-03-18 12:00:33 +0100

    add method "add" that adds two integer numbers to each other and return value as integer, add unit test for mentioned method "add"

--- PW_tests/TestSampleClass.cs
+++ PW_tests/TestSampleClass.cs
@@ -13,5 +13,17 @@ namespace PW_tests
             int result = cs.multiplicate(value);
             Assert.AreEqual(value * 10, result);
         }
+
+        [TestMethod]
+        public void testAdd()
+        {
+            SampleClass cs = new SampleClass();
+            int a = 1;  
+            int b = 2;
+            int result = cs.add(a, b);
+            Assert.AreEqual(a+b, result);
+            Assert.AreNotEqual(a,result);
+            Assert.AreNotEqual(b, result);
+        }
     }
 }
--- Programowanie_wspolbiezne/SampleClass.cs
+++ Programowanie_wspolbiezne/SampleClass.cs
@@ -6,4 +6,9 @@ public class SampleClass
     {
         return value * 10;
     }
+
+    public int add(int value1, int value2)
+    {
+        return value1 + value2;
+    }
 }
