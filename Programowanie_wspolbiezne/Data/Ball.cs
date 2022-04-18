0272f559b857c51e61553d01cdba4d89b532a47c
Michal Banasiak <bananiwo@github.com>
2022-04-18 14:20:47 +0200

    implement basic case of INotifyPropertyChanged

---
+++ Programowanie_wspolbiezne/Data/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Ball
+    {
+        double xPos;
+        double yPos;
+
+        public Ball(double x, double y)
+        {
+            this.xPos = x; 
+            this.yPos = y;
+        }
+ 
+    }
+}
---
+++ Programowanie_wspolbiezne/Presentation/ViewModel/ClassViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Presentation.Model;
+
+namespace Presentation.ViewModel
+{
+    internal class ClassViewModel : INotifyPropertyChanged
+    {
+        ClassModel model;
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return model.ImportantInfo;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        /// <summary>
+        /// Raises OnPropertychangedEvent when property changes
+        /// </summary>
+        /// <param name="name">String representing the property name</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ClassViewModel()
+        {
+            model = new ClassModel();
+            helloString = model.ImportantInfo;
+        }
+
+    }
+}
