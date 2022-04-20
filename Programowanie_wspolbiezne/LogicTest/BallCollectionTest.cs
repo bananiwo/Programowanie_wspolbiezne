38602fc9e6bffb64799ff1c1a37084633661ef5c
Magdalena Malinowska <236596@edu.p.lodz.pl>
2022-04-20 21:53:29 +0200

    add test for logic layer

---
+++ Programowanie_wspolbiezne/LogicTest/BallCollectionTest.cs
@@ -0,0 +1,20 @@
+﻿using Logic;
+using Data;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class BallCollectionTest
+    {
+        [TestMethod]
+        public void TestCreateBallCollection()
+        {
+            BallLogic ballLogic = new BallLogic(740, 740);
+            ballLogic.CreateBallCollection(10);
+            Assert.IsTrue(ballLogic.GetBallCollection().Count == 10);
+
+        } 
+    }
+}
---
+++ Programowanie_wspolbiezne/LogicTest/MovementStepsTest.cs
@@ -0,0 +1,23 @@
+﻿using Logic;
+using Data;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class MovementStepsTest
+    {
+        [TestMethod]
+        public void TestCalculateMovementSteps()
+        {
+            BallLogic ballLogic = new BallLogic(740, 740);
+            Vector2 firstPos = new Vector2(20, 35);
+            Vector2 secondPos = new Vector2(2, 5);
+            int stepsCounter = 2;
+            Vector2 movementSteps = ballLogic.NextStepPosition(firstPos, secondPos, stepsCounter);
+            Assert.AreEqual(movementSteps.X, ((secondPos.X - firstPos.X)/stepsCounter) + firstPos.X);
+            Assert.AreEqual(movementSteps.Y, ((secondPos.Y - firstPos.Y)/stepsCounter) + firstPos.Y);
+        }
+    }
+}
