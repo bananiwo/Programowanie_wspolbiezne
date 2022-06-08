5fd8b43a96391c5b3e57b7d3374a584d65d9f4ba
Magdalena Malinowska <236596@edu.p.lodz.pl>
2022-06-08 18:24:09 +0200

    rebuild model

--- Programowanie_wspolbiezne/PresentationMVM/Model/ModelAbstractApi.cs
+++ Programowanie_wspolbiezne/PresentationMVM/Model/ModelAbstractApi.cs
@@ -20,37 +20,4 @@ namespace PresentationMVM.Model
             return new ModelApi(Weight, Height);
         }
     }
-    internal class ModelApi : ModelAbstractApi
-    {
-        public override int width { get; }
-        public override int height { get; }
-        private readonly LogicAbstractApi LogicLayer;
-
-        public ModelApi(int Width, int Height)
-        {
-
-            width = Width;
-            height = Height;
-            LogicLayer = LogicAbstractApi.CreateApi(width, height);
-        }
-
-        public override void StartMoving()
-        {
-            LogicLayer.Start();
-        }
-
-
-        public override void Stop()
-        {
-            LogicLayer.Stop();
-        }
-
-        public override IList Start(int ballVal) => LogicLayer.CreateBalls(ballVal);
-
-        public override void ClearBalls()
-        {
-            LogicLayer.ClearBalls();
-        }
-    }
-
 }
