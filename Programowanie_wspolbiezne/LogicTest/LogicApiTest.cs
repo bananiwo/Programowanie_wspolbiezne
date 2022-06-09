1c785814b565ce5e4f474868558986df6bad2456
Michal Banasiak <236494@edu.p.lodz.pl>
2022-06-09 22:53:43 +0200

    add tests

--- Programowanie_wspolbiezne/LogicTest/LogicApiTest.cs
+++ Programowanie_wspolbiezne/LogicTest/LogicApiTest.cs
@@ -6,13 +6,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LogicTest
 {
     [TestClass]
-    public class MovementStepsTest
+    public class LogicApiTest
     {
+        LogicAbstractApi api;
 
         [TestMethod]
-        public void TestCalculateMovementSteps()
+        public void TestWidthAndHeight()
         {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            Assert.AreEqual(api.Height, 300);
+            Assert.AreEqual(api.Width, 400);
+        }
 
+        [TestMethod]
+        public void TestCreateBalls()
+        {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            api.CreateBalls(4);
+            Assert.AreEqual(api.GetBallCounter, 4);
+        }
+
+        [TestMethod]
+        public void TestCleareBalls()
+        {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            api.CreateBalls(4);
+            Assert.AreEqual(api.GetBallCounter, 4);
+            api.ClearBalls();
+            Assert.AreEqual(api.GetBallCounter, 0);
         }
     }
 }
