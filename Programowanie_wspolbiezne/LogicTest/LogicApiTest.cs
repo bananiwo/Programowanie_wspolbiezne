9c82a21752387140847e43080394a39e9c0d8a92
Michal Banasiak <236494@edu.p.lodz.pl>
2022-06-09 18:50:09 +0200

    add tests for LogicApi

--- Programowanie_wspolbiezne/LogicTest/LogicApiTest.cs
+++ Programowanie_wspolbiezne/LogicTest/LogicApiTest.cs
@@ -8,11 +8,32 @@ namespace LogicTest
     [TestClass]
     public class MovementStepsTest
     {
+        LogicAbstractApi api;
 
         [TestMethod]
-        public void TestCalculateMovementSteps()
+        public void TestWidthAndHeight()
         {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            Assert.AreEqual(api.Height, 300);
+            Assert.AreEqual(api.Width, 400);
+        }
 
+        [TestMethod]
+        public void TestCreateBalls()
+        {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            api.CreateBalls(4);
+            Assert.AreEqual(api.GetBallCounter, 4);
+        }
+
+        [TestMethod]
+        public void TestCleareBalls()
+        {
+            api = LogicAbstractApi.CreateLogicApi(400, 300);
+            api.CreateBalls(4);
+            Assert.AreEqual(api.GetBallCounter, 4);
+            api.ClearBalls();
+            Assert.AreEqual(api.GetBallCounter, 0);
         }
     }
 }
